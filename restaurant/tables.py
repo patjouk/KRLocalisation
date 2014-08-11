1505748124e9c071786ad6c3ff2a8f5488affc35
Lucie Daeye <lucie.daeye@gmail.com>
2014-08-11 16:59:20 +0200

    Changement de la méthode de sélection des colonnes et retour de la CSS

--- restaurant/tables.py
+++ restaurant/tables.py
@@ -4,4 +4,5 @@ from .models import Restaurant
 class TableauRestaurant(tables.Table):
     class Meta:
         model = Restaurant
-        fields = ('nom', 'nom_coreen', 'code_postal', 'adresse', 'num_tel', 'num_siret', 'proprietaire', 'proprietaire_coreen', 'date_immatriculation', 'date_radiation', 'site_web', 'statut_societe', 'contact', 'interview', 'annuaire_alloparis', 'annuaire_hansik', 'radiation', 'nolebang', 'notes')
+        exclude = ('num_entree', 'latitude', 'longitude')
+        attrs = {"class": "paleblue"}
--- restaurant/views.py
+++ restaurant/views.py
@@ -4,6 +4,6 @@ from .models import Restaurant
 from .tables import TableauRestaurant
 
 def tableau_donnees(request):
-        tabrestaurants = TableauRestaurant(Restaurant.objects.order_by('num_entree').all())
-        RequestConfig(request).configure(tabrestaurants)
+        tabrestaurants = TableauRestaurant(Restaurant.objects.order_by('nom').all())
+        RequestConfig(request, paginate={"per_page":150}).configure(tabrestaurants)
         return render(request, 'restaurant/tableau_donnees.html', {"restaurants": tabrestaurants})
