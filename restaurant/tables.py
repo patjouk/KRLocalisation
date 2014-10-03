1f8fcb384b8aac88075519228c2c84e1e0c23061
nokomis <lucie.daeye@gmail.com>
2014-10-03 13:29:02 +0200

    Trie des variables importantes pour le tableau de la page principale. Mise à jour des fonctions de tri en fonction des variables retenues.

--- restaurant/tables.py
+++ restaurant/tables.py
@@ -5,10 +5,10 @@ from .models import Restaurant
 class TableauRestaurant(tables.Table):
     class Meta:
         model = Restaurant
-        exclude = ('num_entree', 'latitude', 'longitude', 'geom')
+        exclude = ('num_entree', 'latitude', 'longitude', 'geom', 'num_siret', 'proprietaire', 'proprietaire_coreen', 'statut_societe', 'annuaire_alloparis', 'annuaire_hansik', 'nolebang', 'notes', 'site_web')
         attrs = {"class": "paleblue"}
 
 class FiltreRestaurant(django_filters.FilterSet):
     class Meta:
         model = Restaurant
-        fields = ['code_postal', 'statut_societe', 'contact', 'interview', 'annuaire_alloparis', 'annuaire_hansik', 'radiation', 'nolebang']
+        fields = ['code_postal', 'radiation', 'contact', 'interview']
