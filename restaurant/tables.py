c5aa52f7109800290230cf6c983ea162f40d8014
nokomis <lucie.daeye@gmail.com>
2014-10-06 19:42:40 +0200

    Création de la colonne de liens vers les pages details des restaurants.

--- restaurant/tables.py
+++ restaurant/tables.py
@@ -1,8 +1,11 @@
 import django_filters
 import django_tables2 as tables
 from .models import Restaurant
+from django_tables2.utils import A
 
 class TableauRestaurant(tables.Table):
+    nom = tables.LinkColumn('detail', args=[A('pk')])
+
     class Meta:
         model = Restaurant
         exclude = ('num_entree', 'latitude', 'longitude', 'geom', 'num_siret', 'proprietaire', 'proprietaire_coreen', 'statut_societe', 'annuaire_alloparis', 'annuaire_hansik', 'nolebang', 'notes', 'site_web')
